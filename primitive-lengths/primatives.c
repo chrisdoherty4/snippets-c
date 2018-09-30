52b838684b374982ca3409fe52c59888a06bc0dd
Chris Doherty <chris.doherty4@gmail.com>
2018-09-30 12:45:29 +0100

    Add float and double to primatives

--- primitive-lengths/primatives.c
+++ primitive-lengths/primatives.c
@@ -2,14 +2,19 @@
 #include <string.h>
 
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-    printf("%-12s%-12s\n", "Type", "Length");
-    printf("%-12s%-12d\n", "char", sizeof(char));
-    printf("%-12s%-12d\n", "short", sizeof(short));
-    printf("%-12s%-12d\n", "int", sizeof(int));
-    printf("%-12s%-12d\n", "long", sizeof(long));
-    printf("%-12s%-12d\n", "long long", sizeof(long long));
-   
+    char *format = "%-12s%-6d%-6d\n";
+
+    printf("%-12s%-6s%-6s\n", "Type", "Bytes", "Bits");
+    printf(format, "char", sizeof(char), sizeof(char) * 8);
+    printf(format, "short", sizeof(short), sizeof(short) * 8);
+    printf(format, "int", sizeof(int), sizeof(int) * 8);
+    printf(format, "long", sizeof(long), sizeof(long) * 8);
+    printf(format, "long long", sizeof(long long), sizeof(long long) * 8);
+    printf(format, "float", sizeof(float), sizeof(float) * 8);
+    printf(format, "double", sizeof(double), sizeof(double) * 8);
+    printf(format, "long double", sizeof(long double), sizeof(long double) * 8);
+
     return 0;
 }
